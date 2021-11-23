ade8ba168abff562e99d5967ec577f7e2d6a673e
robin vanelven <robin.vanelven@gmail.com>
2021-11-23 15:31:27 +0100

    ex finished

--- TaxiFareModel/trainer.py
+++ TaxiFareModel/trainer.py
@@ -6,7 +6,14 @@ from sklearn.preprocessing import StandardScaler
 from TaxiFareModel.encoders import DistanceTransformer, TimeFeaturesEncoder
 from TaxiFareModel.utils import compute_rmse
 from TaxiFareModel.data import get_data, clean_data, define_y, define_X
+import mlflow
+from mlflow.tracking import MlflowClient
+from memoized_property import memoized_property
 from sklearn.model_selection import train_test_split
+
+estimat = LinearRegression()
+MLFLOW_URI = "https://mlflow.lewagon.co/"
+EXPERIMENT_NAME = "[FR] [LILLE] [Rvanelven] LinearRegression v1"
 
 
 class Trainer():
@@ -15,13 +22,13 @@ class Trainer():
             X: pandas DataFrame
             y: pandas Series
         """
-        self.pipeline = None
+        self.experiment_name = EXPERIMENT_NAME
+        self.pipe = None
         self.X = X
         self.y = y
 
     def split_data(self):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
-            self.X, self.y, test_size=0.2)
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2)
         return self
 
     def set_pipeline(self):
@@ -37,21 +44,44 @@ class Trainer():
         ]), ('time', time_pipe, ['pickup_datetime'])],
                                          remainder="drop")
         self.pipe = Pipeline([('preproc', preproc_pipe),
-                              ('linear_model', LinearRegression())])
+                              ('linear_model', estimat)])
         return self.pipe
 
     def run(self):
         """set and train the pipeline"""
         self.set_pipeline()
-        self.pipe.fit(self.X, self.y)
+        self.pipe.fit(self.X_train, self.y_train)
         return self.pipe
 
-    def evaluate(self, X_test, y_test):
+    def evaluate(self):
         """evaluates the pipeline on df_test and return the RMSE"""
-        y_pred = self.pipe.predict(X_test)
-        rmse = compute_rmse(y_pred, y_test)
-        print(rmse)
+        y_pred = self.pipe.predict(self.X_test)
+        rmse = compute_rmse(y_pred, self.y_test)
         return rmse
+
+    @memoized_property
+    def mlflow_client(self):
+        mlflow.set_tracking_uri(MLFLOW_URI)
+        return MlflowClient()
+
+    @memoized_property
+    def mlflow_experiment_id(self):
+        try:
+
+            return self.mlflow_client.create_experiment(self.experiment_name)
+        except BaseException:
+
+            return self.mlflow_client.get_experiment_by_name(self.experiment_name).experiment_id
+
+    @memoized_property
+    def mlflow_run(self):
+        return self.mlflow_client.create_run(self.mlflow_experiment_id)
+
+    def mlflow_log_param(self, key, value):
+        self.mlflow_client.log_param(self.mlflow_run.info.run_id, key, value)
+
+    def mlflow_log_metric(self, key, value):
+        self.mlflow_client.log_metric(self.mlflow_run.info.run_id, key, value)
 
 
 if __name__ == "__main__":
@@ -62,3 +92,6 @@ if __name__ == "__main__":
     trainer.set_pipeline()
     trainer.run()
     trainer.evaluate()
+    score = trainer.evaluate()
+    trainer.mlflow_log_metric('rmse', score)
+    trainer.mlflow_log_param('estimator', estimat)
